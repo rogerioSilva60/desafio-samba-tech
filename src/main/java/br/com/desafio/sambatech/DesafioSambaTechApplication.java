d857017a535f892d905fca15323702ef629932b7
Rogerio da Silva Araujo <rogeriosilva60@gmail.com>
2020-09-10 23:39:03 -0300

    Projeto inicial

---
+++ src/main/java/br/com/desafio/sambatech/DesafioSambaTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.sambatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSambaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSambaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/sambatech/DesafioSambaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.sambatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSambaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
