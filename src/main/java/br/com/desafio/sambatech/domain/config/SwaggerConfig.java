dfc1e960109da600619c42743b534fdbdc26dd47
Rogerio da Silva Araujo <rogeriosilva60@gmail.com>
2020-09-13 17:32:49 -0300

    Adicionando configuracao e mapeamento dos servicos para documentacao do swagger

---
+++ src/main/java/br/com/desafio/sambatech/domain/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package br.com.desafio.sambatech.domain.config;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.desafio.sambatech.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .directModelSubstitute(Timestamp.class, Long.class)
+                .useDefaultResponseMessages(false);
+    }
+	
+	private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Samba Tech API")
+                .description("API REST Samba Tech")
+                .version("1.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(contact())
+                .build();
+    }
+	
+	private Contact contact(){
+        return new Contact(
+                "Rogerio Silva",
+                "https://github.com/rogerioSilva60",
+                "rogeriosilva60@gmail.com"
+        );
+    }
+}
