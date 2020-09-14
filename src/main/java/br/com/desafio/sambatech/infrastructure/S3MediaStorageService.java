79d75e577a64ed2acc38c1db52d8f5b43f8035b9
Rogerio da Silva Araujo <rogeriosilva60@gmail.com>
2020-09-14 09:35:48 -0300

    Adicionando tratamento de erro para envio do arquivo no S3 e implementando o de deletar arquivo.

--- src/main/java/br/com/desafio/sambatech/infrastructure/S3MediaStorageService.java
+++ src/main/java/br/com/desafio/sambatech/infrastructure/S3MediaStorageService.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import br.com.desafio.sambatech.core.storage.StorageProperties;
 import br.com.desafio.sambatech.domain.service.MediaStorageService;
+import br.com.desafio.sambatech.domain.util.exception.StorageException;
 
 
 public class S3MediaStorageService implements MediaStorageService {
@@ -23,25 +25,37 @@ public class S3MediaStorageService implements MediaStorageService {
 	
 	@Override
 	public void armazenar(NovaMedia novaMedia) {
-		String caminhoArquivo = prepararArquivo(novaMedia.getNome());
-		
-		ObjectMetadata objectMetadata = new ObjectMetadata();
-		objectMetadata.setContentType(novaMedia.getContentType());
-		
-		PutObjectRequest putObjectRequest = new PutObjectRequest(
-				storageProperties.getS3().getBucket(),
-				caminhoArquivo,
-				novaMedia.getInputStream(),
-				objectMetadata)
-		.withCannedAcl(CannedAccessControlList.PublicRead);
-		
-		amanzonS3.putObject(putObjectRequest);
+		try {
+			String caminhoArquivo = prepararArquivo(novaMedia.getNome());
+			
+			ObjectMetadata objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentType(novaMedia.getContentType());
+			
+			PutObjectRequest putObjectRequest = new PutObjectRequest(
+					storageProperties.getS3().getBucket(),
+					caminhoArquivo,
+					novaMedia.getInputStream(),
+					objectMetadata)
+			.withCannedAcl(CannedAccessControlList.PublicRead);
+			
+			amanzonS3.putObject(putObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possivel enviar arquivo para Amazon S3.", e);
+		}
 	}
 
 	@Override
 	public void deletar(String nomeArquivo) {
-		// TODO Auto-generated method stub
-		
+		try { 
+			String caminhoArquivo = prepararArquivo(nomeArquivo);
+			
+			DeleteObjectRequest deleteObjectRequest = 
+					new DeleteObjectRequest(storageProperties.getS3().getBucket(), caminhoArquivo);
+			
+			amanzonS3.deleteObject(deleteObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possivel deletar arquivo para Amazon S3.", e);
+		}
 	}
 
 	@Override
